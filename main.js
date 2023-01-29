9b4ea99f26f0055d85a21bb3d121181e82d29279
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2023-01-29 03:18:10 +0300

    editing

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let cities = [
+  "اختر المحافظه",
   "الشرقية",
   "البحيرة",
   "الفيوم",
@@ -16,40 +17,42 @@ let cities = [
   "أسوان",
   "الدقهلية",
 ];
+let country = document.querySelector(".country");
 
 for (let city of cities) {
   const content = `
-                <option value="" >/${city}</option>
+                <option>${city}</option>
                 `;
   document.getElementById("cities").innerHTML += content;
 }
 
-document.getElementById("cities").addEventListener("change",function Prayer() {
-    this.value === city
-    fetch(
-      `http://api.aladhan.com/v1/timingsByCity/:date_or_timestamp?country=EG&city=${city}`
-    )
-      .then((response) => {
-        response = response.json();
-        return response;
-      })
-      .then((data) => {
-        console.log(data);
-        const timings = data.data.timings;
-        document.getElementById("fajr-time").innerHTML = timings.Fajr;
-        document.getElementById("Sunrise-time").innerHTML = timings.Sunrise;
-        document.getElementById("Dhuhr-time").innerHTML = timings.Dhuhr;
-        document.getElementById("Asr-time").innerHTML = timings.Asr;
-        document.getElementById("Maghrib-time").innerHTML = timings.Maghrib;
-        document.getElementById("Isha-time").innerHTML = timings.Isha;
-    
-        const date =
-          data.data.date.hijri.weekday.ar + "  " + "  " + data.data.date.readable;
-        document.querySelector(".date").innerHTML = date;
-      })
-      .catch((err) => {
-        console.log("ERROR", err.message);
-      });
-    
-})
-Prayer()
+document.getElementById("cities").addEventListener("change", function (e) {
+  console.log(e.target.value);
+
+  country.innerHTML = e.target.value;
+
+  fetch(
+    `http://api.aladhan.com/v1/timingsByCity/:date_or_timestamp?country=EG&city=${e.target.value}`
+  )
+    .then((response) => {
+      response = response.json();
+      return response;
+    })
+    .then((data) => {
+      console.log(data);
+      const timings = data.data.timings;
+      document.getElementById("fajr-time").innerHTML = timings.Fajr;
+      document.getElementById("Sunrise-time").innerHTML = timings.Sunrise;
+      document.getElementById("Dhuhr-time").innerHTML = timings.Dhuhr;
+      document.getElementById("Asr-time").innerHTML = timings.Asr;
+      document.getElementById("Maghrib-time").innerHTML = timings.Maghrib;
+      document.getElementById("Isha-time").innerHTML = timings.Isha;
+
+      const date =
+        data.data.date.hijri.weekday.ar + "  " + "  " + data.data.date.readable;
+      document.querySelector(".date").innerHTML = date;
+    })
+    .catch((err) => {
+      console.log("ERROR", err.message);
+    });
+});
